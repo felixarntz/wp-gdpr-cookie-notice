dca93ec73c57fded3020f5d37e5dc9b74bdd8c3a
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-05-28 23:49:07 +0200

    Implement methods for retrieving option values in plugin settings service.

--- src/settings/class-plugin-settings.php
+++ src/settings/class-plugin-settings.php
@@ -19,6 +19,61 @@ use Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts\Setting_Registry;
 class Plugin_Settings implements Service {
 
 	/**
+	 * Identifier for the plugin's aggregate setting.
+	 */
+	const SETTING_ID = 'wp_gdpr_cookie_notice';
+
+	/**
+	 * Internal storage for the plugin options.
+	 *
+	 * @since 1.0.0
+	 * @var array
+	 */
+	protected $options = [];
+
+	/**
+	 * Internal flag for whether the options in the internal storage are outdated.
+	 *
+	 * @since 1.0.0
+	 * @var bool
+	 */
+	protected $options_dirty = false;
+
+	/**
+	 * Gets a single plugin option value.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $id Option identifier.
+	 * @return mixed Option value, or null if invalid option.
+	 */
+	public function get_option( string $id ) {
+		$options = $this->get_options();
+
+		if ( ! isset( $options[ $id ] ) ) {
+			return null;
+		}
+
+		return $options[ $id ];
+	}
+
+	/**
+	 * Gets all plugin option values.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array Options as $option => $value pairs.
+	 */
+	public function get_options() : array {
+		if ( $this->options_dirty ) {
+			$this->options       = (array) get_option( self::SETTING_ID );
+			$this->options_dirty = false;
+		}
+
+		return $this->options;
+	}
+
+	/**
 	 * Initializes the class functionality.
 	 *
 	 * @since 1.0.0
@@ -26,7 +81,17 @@ class Plugin_Settings implements Service {
 	public function initialize() {
 		add_action( 'init', function() {
 			$this->register_settings();
+
+			$this->options_dirty = true;
 		}, 10, 0 );
+
+		$make_dirty = function() {
+			$this->options_dirty = true;
+		};
+
+		add_action( 'add_option_' . self::SETTING_ID, $make_dirty, 10, 0 );
+		add_action( 'update_option_' . self::SETTING_ID, $make_dirty, 10, 0 );
+		add_action( 'delete_option_' . self::SETTING_ID, $make_dirty, 10, 0 );
 	}
 
 	/**
@@ -35,9 +100,7 @@ class Plugin_Settings implements Service {
 	 * @since 1.0.0
 	 */
 	protected function register_settings() {
-		$id = 'wp_gdpr_cookie_notice';
-
-		$setting_registry = new Aggregate_Setting( $id, [
+		$setting_registry = new Aggregate_Setting( self::SETTING_ID, [
 			Aggregate_Setting::ARG_DESCRIPTION => __( 'Settings for the WP GDPR Cookie Notice plugin.', 'wp-gdpr-cookie-notice' ),
 		] );
 
@@ -52,7 +115,7 @@ class Plugin_Settings implements Service {
 		 */
 		do_action( 'wp_gdpr_cookie_notice_register_settings', $setting_registry );
 
-		( new WordPress_Setting_Registry( $id ) )->register( $id, $setting_registry );
+		( new WordPress_Setting_Registry( self::SETTING_ID ) )->register( self::SETTING_ID, $setting_registry );
 	}
 
 	/**
