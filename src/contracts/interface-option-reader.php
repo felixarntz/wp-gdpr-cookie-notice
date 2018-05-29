6228912e33e2f97133fdd7c1be1df0c78fec594e
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-05-29 17:36:54 +0200

    Introduce option reader interface and use data repository in plugin settings service.

---
+++ src/contracts/interface-option-reader.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts\Option_Reader interface
+ *
+ * @package WP_GDPR_Cookie_Notice
+ * @since 1.0.0
+ */
+
+namespace Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts;
+
+/**
+ * Interface for a class to read options.
+ *
+ * @since 1.0.0
+ */
+interface Option_Reader {
+
+	/**
+	 * Gets a single option value.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $id Option identifier.
+	 * @return mixed Option value, or null if invalid option.
+	 */
+	public function get_option( string $id );
+
+	/**
+	 * Gets all option values.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array Options as $option => $value pairs.
+	 */
+	public function get_options() : array;
+}
--- src/settings/class-plugin-settings.php
+++ src/settings/class-plugin-settings.php
@@ -10,18 +10,29 @@ namespace Leaves_And_Love\WP_GDPR_Cookie_Notice\Settings;
 
 use Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts\Service;
 use Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts\Setting_Registry;
+use Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts\Option_Reader;
+use Leaves_And_Love\WP_GDPR_Cookie_Notice\Contracts\Data_Repository;
+use Leaves_And_Love\WP_GDPR_Cookie_Notice\Data\WordPress_Option_Data_Repository;
 
 /**
  * Class for registering plugin settings.
  *
  * @since 1.0.0
  */
-class Plugin_Settings implements Service {
+class Plugin_Settings implements Service, Option_Reader {
 
 	/**
 	 * Identifier for the plugin's aggregate setting.
 	 */
 	const SETTING_ID = 'wp_gdpr_cookie_notice';
+
+	/**
+	 * Options data repository.
+	 *
+	 * @since 1.0.0
+	 * @var Data_Repository
+	 */
+	protected $data_repository;
 
 	/**
 	 * Internal storage for the plugin options.
@@ -40,7 +51,24 @@ class Plugin_Settings implements Service {
 	protected $options_dirty = false;
 
 	/**
-	 * Gets a single plugin option value.
+	 * Constructor.
+	 *
+	 * Sets the data repository and cookie types to use.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param Data_Repository $data_repository Optional. Data repository to use.
+	 */
+	public function __construct( Data_Repository $data_repository = null ) {
+		if ( null === $data_repository ) {
+			$data_repository = new WordPress_Option_Data_Repository();
+		}
+
+		$this->data_repository = $data_repository;
+	}
+
+	/**
+	 * Gets a single option value.
 	 *
 	 * @since 1.0.0
 	 *
@@ -58,7 +86,7 @@ class Plugin_Settings implements Service {
 	}
 
 	/**
-	 * Gets all plugin option values.
+	 * Gets all option values.
 	 *
 	 * @since 1.0.0
 	 *
@@ -66,7 +94,7 @@ class Plugin_Settings implements Service {
 	 */
 	public function get_options() : array {
 		if ( $this->options_dirty ) {
-			$this->options       = (array) get_option( self::SETTING_ID );
+			$this->options       = $this->data_repository->get( self::SETTING_ID );
 			$this->options_dirty = false;
 		}
 
@@ -104,8 +132,6 @@ class Plugin_Settings implements Service {
 			Aggregate_Setting::ARG_DESCRIPTION => __( 'Settings for the WP GDPR Cookie Notice plugin.', 'wp-gdpr-cookie-notice' ),
 		] );
 
-		$this->register_default_settings( $setting_registry );
-
 		/**
 		 * Fires when the plugin's settings are registered.
 		 *
@@ -116,16 +142,5 @@ class Plugin_Settings implements Service {
 		do_action( 'wp_gdpr_cookie_notice_register_settings', $setting_registry );
 
 		( new WordPress_Setting_Registry( self::SETTING_ID ) )->register( self::SETTING_ID, $setting_registry );
-	}
-
-	/**
-	 * Registers the plugin's default settings.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param Setting_Registry $setting_registry Setting registry to register plugin settings with.
-	 */
-	protected function register_default_settings( Setting_Registry $setting_registry ) {
-
 	}
 }
