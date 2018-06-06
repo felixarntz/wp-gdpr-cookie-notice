c654cd83c1e337c0cf7e5090aec1129c746b3fff
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-06-06 20:38:54 +0200

    Fix unit tests bootstrap script.

--- tests/phpunit/bootstrap-unit-tests.php
+++ tests/phpunit/bootstrap-unit-tests.php
@@ -12,7 +12,7 @@ require_once TESTS_PLUGIN_DIR . '/vendor/autoload.php';
 require_once TESTS_PLUGIN_DIR . '/src/class-autoloader.php';
 
 $autoloader = new Leaves_And_Love\WP_GDPR_Cookie_Notice\Autoloader();
-$autoloader->register_rule( 'Leaves_And_Love\\WP_GDPR_Cookie_Notice', $basedir );
-$autoloader->register_rule( 'Leaves_And_Love\\WP_GDPR_Cookie_Notice\\Contracts', $basedir . '/contracts', Leaves_And_Love\WP_GDPR_Cookie_Notice\Autoloader::TYPE_INTERFACE );
+$autoloader->register_rule( 'Leaves_And_Love\\WP_GDPR_Cookie_Notice', TESTS_PLUGIN_DIR . '/src' );
+$autoloader->register_rule( 'Leaves_And_Love\\WP_GDPR_Cookie_Notice\\Contracts', TESTS_PLUGIN_DIR . '/src/contracts', Leaves_And_Love\WP_GDPR_Cookie_Notice\Autoloader::TYPE_INTERFACE );
 $autoloader->register_rule( 'Leaves_And_Love\\WP_GDPR_Cookie_Notice\\Tests', __DIR__ );
 spl_autoload_register( array( $autoloader, 'load' ) );
